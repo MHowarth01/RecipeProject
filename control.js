3a672cc5cd1c48c9ec73aaeca61468a078809def
MHowarth01 <59856306+MHowarth01@users.noreply.github.com>
2022-05-12 15:28:31 -0400

    Add files via upload

---
+++ control.js
@@ -0,0 +1,13 @@
+//Main
+$(document).ready( function() {
+    console.log("Ready!");
+
+    //Splash-Screen
+    // const splash = document.querySelector('.splash');
+    // console.log("Enter Splash");
+    // setTimeout(()=> {
+    //   console.log("Test");
+    //   splash.classList.add('display-none');
+    // }, 1000); //change later to 3000
+
+});
